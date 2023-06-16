a467b4edd3dcf98a3ec16684dc8e1fc2ec6432d7
Changyu929 <134454015+Changyu929@users.noreply.github.com>
2023-06-16 14:47:12 -0600

    Delete contact directory

--- contact/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Message
-
-
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone_number', 'content', 'created']
