2c78c6729e5d2eee208ec94417f12c4640e034e0
Changyu929 <134454015+Changyu929@users.noreply.github.com>
2023-06-16 14:47:20 -0600

    Delete harmony_web directory

--- harmony_web/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-# set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'harmony_web.settings')
-app = Celery('harmony_web')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
